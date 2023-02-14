6b13da0ca618f8dccd29b7f96841d96078857c82
Brendan Armstrong <brendanarmstrong@proton.me>
2023-02-13 20:01:34 -0500

    partial test for array window

--- src/lib.rs
+++ src/lib.rs
@@ -2,9 +2,22 @@ pub fn ndarray_ones(n: usize, m: usize) -> Vec<Vec<usize>> {
     vec![vec![1; n]; m]
 }
 
+// TODO Costruct a one dimensional array and make a manual window iterator through it
+pub fn array_window(v: &Vec<usize>, s: usize) -> &Vec<usize> {
+    println!("{:?}", s);
+    v
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn one_d_window() {
+        let arr: Vec<usize> = vec![1, 2, 3, 4];
+        let my_win = array_window(&arr, 2);
+        assert_eq!(arr, *my_win);
+    }
 
     #[test]
     fn it_works() {
