254df03cfceca895ad3ff7264c88127272fe47e6
Brendan Armstrong <brendanarmstrong@proton.me>
2023-02-17 19:41:01 -0500

    feat: established iterator for layer(renamed later)

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,73 @@
-pub fn ndarray_ones(n: usize, m: usize) -> Vec<Vec<usize>> {
-    vec![vec![1; n]; m]
+#[derive(Debug)]
+#[allow(dead_code)]
+pub struct Layer {
+    body: Vec<Vec<usize>>,
 }
 
-pub fn generate_array(n: usize, m: usize) -> Vec<Vec<usize>> {
-    let mut numbers = (1..).into_iter();
-    let mut outline_mat = vec![vec![1usize; n]; m];
-    outline_mat
-        .iter_mut()
-        .map(|inner| {
-            inner
+pub struct LayerIter<'a> {
+    values: &'a Vec<Vec<usize>>,
+    y: usize,
+    x: usize,
+}
+
+impl<'a> Iterator for LayerIter<'a> {
+    type Item = &'a usize;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.x >= self.values[self.y].len() {
+            self.x = 0;
+            self.y += 1;
+            if self.y >= self.values.len() {
+                return None;
+            }
+        }
+        self.x += 1;
+        Some(&self.values[self.y][self.x - 1])
+    }
+}
+
+impl std::fmt::Display for Layer {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        let body = self
+            .body
+            .iter()
+            .map(|row| format!("{:?}", row))
+            .fold(String::new(), |st, row| st + &row + "\n");
+        write!(f, "{}", body)
+    }
+}
+
+#[allow(unused)]
+impl Layer {
+    fn new(n: usize, m: usize) -> Self {
+        let mut numbers = (1..).into_iter().map(|v| v % 10);
+        let mut outline_mat = vec![vec![1usize; n]; m];
+        return Layer {
+            body: outline_mat
                 .iter_mut()
-                .map(|_| numbers.next().unwrap())
-                .collect::<Vec<usize>>()
-        })
-        .collect::<Vec<Vec<usize>>>()
+                .map(|inner| {
+                    inner
+                        .iter_mut()
+                        .map(|_| numbers.next().unwrap())
+                        .collect::<Vec<usize>>()
+                })
+                .collect::<Vec<Vec<usize>>>(),
+        };
+    }
+    fn iter(&self) -> impl Iterator<Item = &usize> {
+        LayerIter {
+            values: &self.body,
+            x: 0,
+            y: 0,
+        }
+    }
+}
+
+pub fn mat_window(arr: &Layer) -> &Layer {
+    println!("{}", &arr);
+    let arr_sum: usize = arr.iter().sum();
+    println!("{}", arr_sum);
+    &arr
 }
 
 #[cfg(test)]
@@ -21,10 +75,19 @@ mod tests {
     use super::*;
 
     #[test]
-    fn array_test() {
-        let arr = generate_array(4, 4);
+    fn test_window_iterator() {
+        let arr = Layer::new(5, 5);
+        let win = mat_window(&arr);
+        println!("win after borrow:\n{}", win);
+        println!("arr after borrow:\n{}", arr);
+        assert_eq!(&win.body, &vec![[1, 2], [5, 6]]);
+    }
+
+    #[test]
+    fn test_new_layer() {
+        let arr = Layer::new(4, 4);
         assert_eq!(
-            arr,
+            arr.body,
             vec![
                 [1, 2, 3, 4],
                 [5, 6, 7, 8],
@@ -32,18 +95,5 @@ mod tests {
                 [13, 14, 15, 16]
             ]
         );
-    }
-
-    #[test]
-    fn it_works() {
-        let result = ndarray_ones(4, 4);
-        #[rustfmt::skip]
-        let mat = vec![
-            [1, 1, 1, 1],
-            [1, 1, 1, 1],
-            [1, 1, 1, 1],
-            [1, 1, 1, 1],
-        ];
-        assert_eq!(result, mat);
     }
 }
