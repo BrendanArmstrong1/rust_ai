e6fa527edc5809a07ade722f437796beac839963
Brendan Armstrong <brendanarmstrong@proton.me>
2023-02-20 16:37:48 -0500

    feat: iter window is working for "Layer". verified that the address of the values is the same as the original content

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,87 @@
 #[derive(Debug)]
 #[allow(dead_code)]
 pub struct Layer {
-    body: Vec<Vec<usize>>,
+    pub body: Vec<Vec<usize>>,
+}
+
+#[allow(dead_code)]
+impl Layer {
+    pub fn new(n: usize, m: usize) -> Self {
+        let mut numbers = (1..).into_iter();
+        let mut outline_mat = vec![vec![1usize; n]; m];
+        return Layer {
+            body: outline_mat
+                .iter_mut()
+                .map(|inner| {
+                    inner
+                        .iter_mut()
+                        .map(|_| numbers.next().unwrap())
+                        .collect::<Vec<usize>>()
+                })
+                .collect::<Vec<Vec<usize>>>(),
+        };
+    }
+    pub fn iter(&self) -> impl Iterator<Item = &usize> {
+        LayerIter {
+            values: &self.body,
+            x: 0,
+            y: 0,
+        }
+    }
+
+    pub fn iter_win(&self, w: usize, h: usize) -> LayerWindow<'_> {
+        LayerWindow {
+            values: &self.body,
+            x: 0,
+            y: 0,
+            w,
+            h,
+        }
+    }
 }
 
 pub struct LayerIter<'a> {
     values: &'a Vec<Vec<usize>>,
     y: usize,
     x: usize,
+}
+
+pub struct LayerWindow<'a> {
+    values: &'a Vec<Vec<usize>>,
+    y: usize,
+    x: usize,
+    w: usize,
+    h: usize,
+}
+
+impl<'a> Iterator for LayerWindow<'a> {
+    type Item = Vec<Vec<&'a usize>>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.x + self.w > self.values[self.y].len() {
+            self.x = 0;
+            self.y += 1;
+            if self.y + self.h > self.values.len() {
+                return None;
+            }
+        }
+
+        let mat_iter = self
+            .values
+            .iter()
+            .skip(self.y)
+            .take(self.h)
+            .map(|row| {
+                row.iter()
+                    .skip(self.x)
+                    .take(self.w)
+                    .collect::<Vec<&usize>>()
+            })
+            .collect::<Vec<Vec<&usize>>>();
+
+        self.x += 1;
+        return Some(mat_iter);
+    }
 }
 
 impl<'a> Iterator for LayerIter<'a> {
@@ -38,38 +112,6 @@ impl std::fmt::Display for Layer {
 }
 
 #[allow(unused)]
-impl Layer {
-    fn new(n: usize, m: usize) -> Self {
-        let mut numbers = (1..).into_iter().map(|v| v % 10);
-        let mut outline_mat = vec![vec![1usize; n]; m];
-        return Layer {
-            body: outline_mat
-                .iter_mut()
-                .map(|inner| {
-                    inner
-                        .iter_mut()
-                        .map(|_| numbers.next().unwrap())
-                        .collect::<Vec<usize>>()
-                })
-                .collect::<Vec<Vec<usize>>>(),
-        };
-    }
-    fn iter(&self) -> impl Iterator<Item = &usize> {
-        LayerIter {
-            values: &self.body,
-            x: 0,
-            y: 0,
-        }
-    }
-}
-
-pub fn mat_window(arr: &Layer) -> &Layer {
-    println!("{}", &arr);
-    let arr_sum: usize = arr.iter().sum();
-    println!("{}", arr_sum);
-    &arr
-}
-
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -77,10 +119,39 @@ mod tests {
     #[test]
     fn test_window_iterator() {
         let arr = Layer::new(5, 5);
-        let win = mat_window(&arr);
-        println!("win after borrow:\n{}", win);
-        println!("arr after borrow:\n{}", arr);
-        assert_eq!(&win.body, &vec![[1, 2], [5, 6]]);
+        let mut arr_win = arr.iter_win(2, 2);
+        assert_eq!(
+            arr_win.next().unwrap(),
+            vec![vec![&1usize, &2usize], vec![&6usize, &7usize]]
+        );
+        arr_win.next();
+        arr_win.next();
+        arr_win.next();
+        assert_eq!(
+            arr_win.next().unwrap(),
+            vec![vec![&6usize, &7usize], vec![&11usize, &12usize]]
+        );
+        arr_win.next();
+        arr_win.next();
+        arr_win.next();
+        assert_eq!(
+            arr_win.next().unwrap(),
+            vec![vec![&11usize, &12usize], vec![&16usize, &17usize]]
+        );
+        arr_win.next();
+        arr_win.next();
+        arr_win.next();
+        assert_eq!(
+            arr_win.next().unwrap(),
+            vec![vec![&16usize, &17usize], vec![&21usize, &22usize]]
+        );
+        arr_win.next();
+        arr_win.next();
+        assert_eq!(
+            arr_win.next().unwrap(),
+            vec![vec![&19usize, &20usize], vec![&24usize, &25usize]]
+        );
+        assert_eq!(arr_win.next(), None);
     }
 
     #[test]
